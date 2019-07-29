9d7d25ef33197106298379eff378e04dbd999173
Wesley Bonneville <westazbonneville@gmail.com>
2019-07-29 14:11:09 -0400

    Complete UI

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -13,11 +13,13 @@ function App() {
       <div className="row">
         <div className="col-10 mx-auto col-md-8 mt-4">
       <h3 className="text-capitalize text-center">todo input</h3>
-      <TodoList />
       <TodoInput />
           </div>
+          
       </div>
+      <TodoList />
     </div>
+    
   );
 }
 
--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
+import TodoItem from "./TodoItem"
 
 export default class TodoList extends Component {
     render() {
         return (
-            <div>
-                <h1>
-                    Hello
-                </h1>
-            </div>
+           <ul className="list-group my-5">
+               <h3 className="text-capitalize text-center">todo list</h3>
+               <TodoItem />
+               <button type="button" className="btn btn-danger btn-block text-capitalize mt-5">clear list</button>
+           </ul>
         )
     }
 }
