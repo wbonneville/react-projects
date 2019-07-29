aa460c325b1675f00b7091a675e8251e311e014c
Wesley Bonneville <westazbonneville@gmail.com>
2019-07-29 14:48:25 -0400

    Add state

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -8,12 +8,26 @@ import uuid from "uuid";
 import './App.css';
 
 function App() {
+  state = {
+    items: [],
+    id: 0,
+    item: "",
+    editItem: false
+  }
+
+  handleChange = e => {
+    this.setState({
+      item: e.target.value
+    })
+  }
   return (
     <div className="container">
       <div className="row">
         <div className="col-10 mx-auto col-md-8 mt-4">
       <h3 className="text-capitalize text-center">todo input</h3>
-      <TodoInput />
+      <TodoInput 
+      item={this.state.item} handleChange={this.handleChange}
+      />
           </div>
           
       </div>
