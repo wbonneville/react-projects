72813e81dfd1b1e2f2b355b8424500756194fe73
Wesley Bonneville <westazbonneville@gmail.com>
2019-07-31 11:10:23 -0400

    Update App.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import TodoItem from "./components/TodoItem";
 import TodoList from "./components/TodoList";
 import TodoInput from "./components/TodoInput";
 import "bootstrap/dist/css/bootstrap.min.css";
