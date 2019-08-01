181a9159be1158c0c2a24de8843165d994d3369c
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-01 15:31:49 -0400

    Bug fix

--- stylecomponents/test/src/App.js
+++ stylecomponents/test/src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
-import "./App.css";
+import ReactDOM from "react-dom";
 import styled, { css } from "styled-components";
+import { Grid, Row, Col } from "react-flexbox-grid";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./Components/Header.js";
 import Showcase from "./Components/Showcase.js";
 import Features from "./Components/Features.js";
 import Infosection from "./Components/Infosection.js";
 import Company from "./Components/Company.js";
 import Footer from "./Components/Footer.js";
-import { Grid, Row, Col } from "react-flexbox-grid";
 
 function App() {
   return (
--- stylecomponents/test/src/Components/Infosection.js
+++ stylecomponents/test/src/Components/Infosection.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 
+// import clipart2 from "./src/images/clipart2.png";
+
 export default class Infosection extends Component {
   render() {
     return (
       <section id="info">
-        <div class="container">
-          <div class="row center-xs center-sm center-md center-lg middle-xs middle-sm middle-md middle-lg">
-            <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-              <img class="clipart" src="images/clipart2.png" alt="" />
+        <div className="container">
+          <div className="row center-xs center-sm center-md center-lg middle-xs middle-sm middle-md middle-lg">
+            <div className="col-xs-12 col-sm-6 col-md-6 col-lg-6">
+              <img className="clipart" alt="" />
             </div>
             <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
               <h2>Core Features</h2>
