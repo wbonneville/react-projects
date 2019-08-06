96c9a812226f1c5a12daf5a47a04dcfb84b0ac24
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-06 09:01:11 -0400

    Update App.js

--- stylecomponents/test/src/App.js
+++ stylecomponents/test/src/App.js
@@ -13,7 +13,7 @@ import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
   body {
-    font-family:'San Francisco', sans-serif;
+    font-family:'Futura', sans-serif;
     font-size: 18px;
     line-height: 1.5em;
     margin: 0;
