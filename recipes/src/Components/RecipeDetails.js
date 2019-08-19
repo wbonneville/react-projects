9cecca6128cf5547f083d496a7081c42e6d90ba3
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-19 09:57:33 -0400

    Add components

---
+++ recipes/src/Components/RecipeDetails.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class RecipeDetails extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello from Details</h1>
+      </React.Fragment>
+    );
+  }
+}
---
+++ recipes/src/Components/RecipeList.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import Recipe from "./Recipe";
+import RecipeSearch from "./RecipeSearch";
+
+export default class RecipeList extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello from List </h1>
+        <RecipeSearch />
+        <Recipe />
+      </React.Fragment>
+    );
+  }
+}
