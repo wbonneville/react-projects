263697cee7419ead1e60f17581730d1b75c7c9dd
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-19 11:06:59 -0400

    Add the api

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -4,12 +4,22 @@ import { recipes } from "./tempList";
 import RecipeList from "./Components/RecipeList";
 import RecipeDetails from "./Components/RecipeDetails";
 
-function App() {
-  return (
-    <React.Fragment>
-      <RecipeList /> <RecipeDetails />
-    </React.Fragment>
-  );
+class App extends Component {
+  state = {
+    recipes: [],
+    url:
+      "https://www.food2fork.com/api/search?key=0f888992f670fc1649857c6c2490cac6&q"
+  };
+
+  async getRecipes() {}
+
+  render() {
+    return (
+      <React.Fragment>
+        <RecipeList /> <RecipeDetails />
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
