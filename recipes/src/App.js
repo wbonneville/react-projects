614fd8c6dacc6c52d2801879e0221c7d5b6843cf
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-19 14:21:13 -0400

    Add data from api

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -11,7 +11,9 @@ class App extends Component {
       "https://www.food2fork.com/api/search?key=0f888992f670fc1649857c6c2490cac6&q"
   };
 
-  async getRecipes() {}
+  async getRecipes() {
+    const data = await fetch(this.state.url);
+  }
 
   render() {
     return (
