61558a6eef50d635409197666f896350e0e2196b
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-20 10:43:24 -0400

    Remove callback limit

--- recipes/src/Components/RecipeList.js
+++ recipes/src/Components/RecipeList.js
@@ -4,10 +4,19 @@ import RecipeSearch from "./RecipeSearch";
 
 export default class RecipeList extends Component {
   render() {
+    const { recipes } = this.props;
     return (
       <React.Fragment>
-        <h1>Hello from List </h1>
         <RecipeSearch />
+        <div className="container my-5">
+          {/* title */}
+          <div className="row">
+            <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+              <h1 className="text-slanted">recipe list</h1>
+            </div>
+          </div>
+          {/* end of title */}
+        </div>
         <Recipe />
       </React.Fragment>
     );
