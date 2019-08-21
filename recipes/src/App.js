a7d1a34fa10e3a3f1f71b5adfe0ec5dcd930cb72
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-21 11:22:33 -0400

    Update App.js

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -8,35 +8,37 @@ class App extends Component {
   state = {
     recipes: recipes,
     url:
-      "https://www.food2fork.com/api/search?key=0f888992f670fc1649857c6c2490cac6&q"
+      "https://www.food2fork.com/api/search?key=0f888992f670fc1649857c6c2490cac6&q",
+    details_id: 35382
   };
 
-  async getRecipes() {
-    // Function to get recipes from API
-    try {
-      // Try this function
-      const data = await fetch(this.state.url); // Data is equal to the current state of the data from the URL
-      const jsonData = await data.json(); // jsonData is equal to data
-      this.setState({
-        // Set recipe state of jsonData.recipes
-        recipes: jsonData.recipes
-      });
-    } catch (error) {
-      // Report back with any erors
-      console.log(error);
-    }
-  }
+  // async getRecipes() {
+  //   // Function to get recipes from API
+  //   try {
+  //     // Try this function
+  //     const data = await fetch(this.state.url); // Data is equal to the current state of the data from the URL
+  //     const jsonData = await data.json(); // jsonData is equal to data
+  //     this.setState({
+  //       // Set recipe state of jsonData.recipes
+  //       recipes: jsonData.recipes
+  //     });
+  //   } catch (error) {
+  //     // Report back with any erors
+  //     console.log(error);
+  //   }
+  // }
 
-  componentDidMount() {
-    this.getRecipes();
-  }
+  // componentDidMount() {
+  //   this.getRecipes();
+  // }
 
   render() {
     // console.log(this.state.recipes);
 
     return (
       <React.Fragment>
-        <RecipeList recipes={this.state.recipes} /> <RecipeDetails />
+        <RecipeList recipes={this.state.recipes} />
+        <RecipeDetails id={this.state.details_id} />
       </React.Fragment>
     );
   }
