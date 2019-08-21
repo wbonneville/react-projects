a7d10623ccc10f18f9587ef92390c8b868864b62
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-21 11:33:29 -0400

    Update RecipeDetails.js

--- recipes/src/Components/RecipeDetails.js
+++ recipes/src/Components/RecipeDetails.js
@@ -1,6 +1,17 @@
 import React, { Component } from "react";
 import { recipe } from "../tempDetails";
 export default class RecipeDetails extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipe: {},
+      url: `https://www.food2fork.com/api/get?key=0f888992f670fc1649857c6c2490cac6&q&rId=${
+        this.props.id
+      }
+        `
+    };
+  }
   render() {
     return (
       <React.Fragment>
