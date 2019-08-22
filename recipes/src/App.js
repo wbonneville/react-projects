e7ebbb238ab58c4f967322df69a655efba44e621
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-22 12:26:34 -0400

    Update App.js

--- recipes/src/App.js
+++ recipes/src/App.js
@@ -9,22 +9,18 @@ class App extends Component {
     recipes: recipes,
     url:
       "https://www.food2fork.com/api/search?key=0f888992f670fc1649857c6c2490cac6&q",
-    details_id: 35380,
+    details_id: 35389,
     pageIndex: 1
   };
 
   async getRecipes() {
-    // Function to get recipes from API
     try {
-      // Try this function
-      const data = await fetch(this.state.url); // Data is equal to the current state of the data from the URL
-      const jsonData = await data.json(); // jsonData is equal to data
+      const data = await fetch(this.state.url);
+      const jsonData = await data.json();
       this.setState({
-        // Set recipe state of jsonData.recipes
         recipes: jsonData.recipes
       });
     } catch (error) {
-      // Report back with any erors
       console.log(error);
     }
   }
