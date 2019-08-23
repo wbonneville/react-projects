1c8a6cfc33159706ace114f5699a9a9c05ff3db3
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-23 13:46:11 -0400

    Update RecipeSearch.js

--- recipes/src/Components/RecipeSearch.js
+++ recipes/src/Components/RecipeSearch.js
@@ -6,7 +6,12 @@ export default class RecipeSearch extends Component {
       <React.Fragment>
         <div className="container">
           <div className="row">
-            <div className="col-10 mx-auto col-md-8 mt-5 text-center" />
+            <div className="col-10 mx-auto col-md-8 mt-5 text-center">
+              <h1 className="text-slanted text-capitalize">
+                search for recipe with{" "}
+                <strong className="text-danger">Food2Fork</strong>
+              </h1>
+            </div>
           </div>
         </div>
       </React.Fragment>
