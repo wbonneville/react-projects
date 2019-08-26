92d2d5a63fb1e10725f00b135f01999befadc642
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-26 14:33:50 -0400

    Add navbar style

--- hl2website/hl2website/src/Components/NavigationBar.js
+++ hl2website/hl2website/src/Components/NavigationBar.js
@@ -1,18 +1,33 @@
 import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import styled from "styled-components";
+
+const StyledWrap = styled(Navbar)`
+  background: rgb(46, 58, 80);
+  opacity: 0.99;
+`;
+
+const StyledBrand = styled(Navbar.Brand)`
+  && {
+    color: white;
+    :hover {
+      color: white;
+    }
+  }
+`;
 
 function NavigationBar() {
   return (
-    <div>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">Half-Life 2 Information Desk</Navbar.Brand>
+    <StyledWrap>
+      <Navbar>
+        <StyledBrand href="#home">City 17 Information Desk</StyledBrand>
         <Nav className="mr-auto">
-          <Nav.Link href="#home">City 17</Nav.Link>
+          <Nav.Link href="#city17">City 17</Nav.Link>
           <Nav.Link href="#features">Apartments and Jobs</Nav.Link>
           <Nav.Link href="#pricing">Pricing</Nav.Link>
         </Nav>
       </Navbar>
-    </div>
+    </StyledWrap>
   );
 }
 
