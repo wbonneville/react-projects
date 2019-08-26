25ec44df5fd9ae75ba57391ec200dde1be1b21e5
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-26 10:42:10 -0400

    update Navbar, add bootstrap

--- halflife-2/halflife2/src/App.js
+++ halflife-2/halflife2/src/App.js
@@ -3,10 +3,12 @@ import "./App.css";
 import Header from "./Components/Header";
 import Gallery from "./Components/Gallery";
 import Info from "./Components/Info";
+import { NavigationBar } from "./Components/NavigationBar";
 
 function App() {
   return (
     <React.Fragment>
+      <NavigationBar />
       <Header></Header>
       <Gallery></Gallery>
       <Info></Info>
