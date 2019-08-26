1c888ab390f5606fb116193d56cd9c151c7796c9
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-26 15:20:44 -0400

    Update SecondaryNavigationBar.js

--- hl2website/hl2website/src/Components/SecondaryNavigationBar.js
+++ hl2website/hl2website/src/Components/SecondaryNavigationBar.js
@@ -2,11 +2,25 @@ import React from "react";
 import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
 import styled from "styled-components";
 
+const StyledWrap = styled(Navbar)`
+  background: white;
+  opacity: 0.99;
+  max-height: 40px;
+`;
+
+const StyledBrand = styled(Navbar.Brand)`
+  && {
+    color: white;
+    :hover {
+      color: white;
+    }
+  }
+`;
+
 function SecondaryNavigationBar() {
   return (
     <StyledWrap>
       <Navbar>
-        <StyledBrand href="#home">City 17 Information Desk</StyledBrand>
         <Nav className="mr-auto">
           <Nav.Link href="#city17">City 17</Nav.Link>
           <Nav.Link href="#features">Apartments and Jobs</Nav.Link>
