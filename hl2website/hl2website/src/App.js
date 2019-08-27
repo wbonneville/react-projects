40da144ce14ad9850e1c4551d85af1899c5fbf31
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-27 13:48:43 -0400

    Add new navbar

--- hl2website/hl2website/src/App.js
+++ hl2website/hl2website/src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import HeadNavigationBar from "./Components/NavigationBar";
 import NavigationBar from "./Components/NavigationBar";
 import SecondaryNavagationBar from "./Components/SecondaryNavigationBar";
 import Slide from "./Components/Slide";
@@ -8,6 +9,7 @@ import Slogan from "./Components/Slogan";
 function App() {
   return (
     <React.Fragment>
+      <HeadNavigationBar />
       <NavigationBar />
       <SecondaryNavagationBar />
       <Slide> </Slide>
---
+++ hl2website/hl2website/src/Components/HeadNavigationBar.js
@@ -0,0 +1,43 @@
+import React from "react";
+import { Navbar, Nav, Form, FormControl, Button } from "react-bootstrap";
+import styled from "styled-components";
+
+// Navbar Style
+const StyledWrap = styled(Navbar)`
+  background: rgb(46, 58, 80);
+  opacity: 0.99;
+  font-family: "Roboto";
+`;
+
+const StyledBrand = styled(Navbar.Brand)`
+  && {
+    color: white;
+    font-size: 22px;
+    :hover {
+      color: white;
+    }
+  }
+`;
+
+const StyledLink = styled(Nav.Link)`
+  &&& {
+    color: white;
+  }
+`;
+
+function NavigationBar() {
+  return (
+    <StyledWrap>
+      <Navbar>
+        <StyledBrand href="#home">
+          <strong>City 17 Information Desk</strong>
+        </StyledBrand>
+        <Nav className="mr-auto">
+          <StyledLink href="#city17">Civil Protection | Metropolice</StyledLink>
+        </Nav>
+      </Navbar>
+    </StyledWrap>
+  );
+}
+
+export default NavigationBar;
