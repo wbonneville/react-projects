61d1bfcb9d6e89b8d79170c2881c9a760467cddf
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-27 11:18:59 -0400

    finally fix more things

--- hl2website/hl2website/src/Components/Slide.js
+++ hl2website/hl2website/src/Components/Slide.js
@@ -1,18 +1,24 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import image from "../images/slide2.jpg";
 
-const ImgWrap = styled.img`
-  height: 100%;
-  width: 100%;
-  opacity: 0.8;
+const ImgWrap = styled.div`
+  background-image: url(${image});
+  background-size: contain;
+  & .row {
+    width: 100%;
+    height: 600px;
+  }
 `;
 
 export default class Index extends Component {
   render() {
     return (
-      <div>
-        <ImgWrap src="https://wallup.net/wp-content/uploads/2017/03/27/381707-minimalism-selective_coloring-Half-Life_2-The_Citadel-Combine-video_games-artwork.jpg"></ImgWrap>
-      </div>
+      <ImgWrap>
+        <div className="container">
+          <div className="row"></div>
+        </div>
+      </ImgWrap>
     );
   }
 }
