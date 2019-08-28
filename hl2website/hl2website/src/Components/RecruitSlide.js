f6db41bbd79e78282283377bba02d5f33e23dafb
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-28 16:37:28 -0400

    Update RecruitSlide.js

--- hl2website/hl2website/src/Components/RecruitSlide.js
+++ hl2website/hl2website/src/Components/RecruitSlide.js
@@ -2,44 +2,72 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const CircleWrapper = styled.div`
-  width: 1000px;
-  margin-left: 500px;
+  text-align: center;
+  padding-bottom: 80px;
+  width: 100%;
+  font-family: "Futura";
+  & h2 {
+    padding-top: 40px;
+    font-size: 12px;
+  }
+
+  & .goldHr {
+    width: 12%;
+    margin-left: auto;
+    margin-right: auto;
+    border-color: #b96835;
 `;
 
 const Circle = styled.div`
-  height: 80px;
-  width: 80px;
-  background-color: #fff;
-  border-radius: 50%;
-  display: table-cell;
-  vertical-align: middle;
-  text-align: center;
+  display: inline-block;
+  padding-top: 70px;
+  & i {
+    margin-right: 60px;
+  }
+
+  & .fa-city {
+    color: black;
+  }
+  & .fa-city {
+    color: black;
+  }
+  & .fa-city {
+    color: black;
+  }
+  & .fa-city {
+    color: black;
+  }
 `;
 
 export default class RecruitSlide extends Component {
   render() {
     return (
       <CircleWrapper>
-        <h1>Join us today</h1>
+        <h1>Recruiting</h1>
+        <hr className="goldHr"></hr>
+        <h2>
+          Success breeds complacency. Complacency breeds failure. Only the
+          paranoid survive.
+        </h2>
         <Circle className="dot">
           {" "}
-          <i class="fas fa-city fa-3x"></i>
+          <i class="fas fa-city fa-5x"></i>
         </Circle>
         <Circle className="dot">
           {" "}
-          <i class="fas fa-clinic-medical fa-3x"></i>
+          <i class="fas fa-clinic-medical fa-5x"></i>
         </Circle>
         <Circle className="dot">
           {" "}
-          <i class="far fa-address-card fa-3x"></i>
+          <i class="far fa-address-card fa-5x"></i>
         </Circle>
         <Circle className="dot">
           {" "}
-          <i class="fas fa-charging-station fa-3x"></i>
+          <i class="fas fa-charging-station fa-5x"></i>
         </Circle>
         <Circle className="dot">
           {" "}
-          <i class="fas fa-chalkboard-teacher fa-3x"></i>
+          <i class="fas fa-chalkboard-teacher fa-5x"></i>
         </Circle>
       </CircleWrapper>
     );
