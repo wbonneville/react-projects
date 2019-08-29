dadf2907729fe55c3c26c75fb07a6e23e7e54eac
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-29 09:58:27 -0400

    Update NewSection.js

--- hl2website/hl2website/src/Components/NewSection.js
+++ hl2website/hl2website/src/Components/NewSection.js
@@ -2,11 +2,37 @@ import React, { Component } from "react";
 import styled from "styled-components";
 
 const SectionWrapper = styled.section`
-  background-color: #b96835;
+  background-color: #313d52;
+  width: 100%;
+  height: 600px;
 `;
 
 export default class NewSection extends Component {
   render() {
-    return <SectionWrapper></SectionWrapper>;
+    return (
+      <SectionWrapper>
+        <h1></h1>
+        <hr></hr>
+        <div>
+          <h2>
+            <hr></hr>
+          </h2>
+          <h2>
+            <hr></hr>
+          </h2>
+          <h2>
+            <hr></hr>
+          </h2>
+          <h3></h3>
+          <h3></h3>
+          <h3></h3>
+          <h3></h3>
+          <h3></h3>
+          <h3></h3>
+          <h3></h3>
+          <h3></h3>
+        </div>
+      </SectionWrapper>
+    );
   }
 }
