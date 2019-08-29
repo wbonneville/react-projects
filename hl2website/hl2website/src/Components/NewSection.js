3ec99db1148c4bf6225e47ab6e7d926e2af6d908
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-29 10:17:02 -0400

    Update NewSection.js

--- hl2website/hl2website/src/Components/NewSection.js
+++ hl2website/hl2website/src/Components/NewSection.js
@@ -5,13 +5,30 @@ const SectionWrapper = styled.section`
   background-color: #313d52;
   width: 100%;
   height: 600px;
+  font-family: "Roboto";
+
+  & h1 {
+    padding-top: 62px;
+    padding-left: 40px;
+    color: white;
+    font-size: 50px;
+
+    & .spanOne,
+    .spanTwo {
+      color: #b96835;
+    }
+  }
 `;
 
 export default class NewSection extends Component {
   render() {
     return (
       <SectionWrapper>
-        <h1></h1>
+        <h1>
+          Leading City 17's structural policies to advance the{" "}
+          <span className="spanOne">interests</span> and
+          <span className="spanTwo"> security</span> of all entities.
+        </h1>
         <hr></hr>
         <div>
           <h2>
