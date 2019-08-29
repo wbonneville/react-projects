22b9908cf7a7e23a0ba724288885d1b92359d022
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-29 09:51:35 -0400

    Update NewSection.js

---
+++ hl2website/hl2website/src/Components/NewSection.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import styled from "styled-components";
+
+const SectionWrapper = styled.section`
+  background-color: #b96835;
+`;
+
+export default class NewSection extends Component {
+  render() {
+    return <SectionWrapper></SectionWrapper>;
+  }
+}
