2a5085c89914d3194e3017a365e3ada4225eae00
Wesley Bonneville <westazbonneville@gmail.com>
2019-08-30 12:00:23 -0400

    Update SlideThree.js

--- hl2website/hl2website/src/Components/SlideThree.js
+++ hl2website/hl2website/src/Components/SlideThree.js
@@ -2,14 +2,34 @@ import React, { Component } from "react";
 import styled from "styled-components";
 // background-color: #f4f4f4ce;
 const SlideWrapper = styled.div`
+  width: 100%;
+  text-align: center;
+  & .slide {
+    display: inline-block;
+  }
+`;
+
+const SlideOne = styled.div`
   background-color: pink;
   width: 40%;
   height: 600px;
   font-family: "Roboto";
 `;
 
+const SlideTwo = styled.div`
+  background-color: red;
+  width: 60%;
+  height: 600px;
+  font-family: "Roboto";
+`;
+
 export default class SlideThree extends Component {
   render() {
-    return <SlideWrapper></SlideWrapper>;
+    return (
+      <SlideWrapper>
+        <SlideOne className="slide"></SlideOne>
+        <SlideTwo className="slide"></SlideTwo>
+      </SlideWrapper>
+    );
   }
 }
