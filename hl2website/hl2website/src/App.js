88781e1f1719110736fbfc83f1fa279766c47fe8
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-01 22:53:33 -0400

    Update App.js

--- hl2website/hl2website/src/App.js
+++ hl2website/hl2website/src/App.js
@@ -10,21 +10,27 @@ import SlideTwo from "./Components/SlideTwo";
 import RecruitSlide from "./Components/RecruitSlide";
 import NewSection from "./Components/NewSection";
 import SlideThree from "./Components/SlideThree";
+import { Grid, Row, Col } from "react-flexbox-grid";
 
 function App() {
   return (
-    <React.Fragment>
-      <Official></Official>
-      <NavigationBar />
-      <SecondaryNavagationBar />
-      <Slide> </Slide>
-      <Slogan></Slogan>
-      <News></News>
-      <RecruitSlide></RecruitSlide>
-      <NewSection></NewSection>
-      <SlideThree></SlideThree>
-      <SlideTwo></SlideTwo>
-    </React.Fragment>
+    <Grid fluid>
+      <Row>
+        <Col xs={12}>
+          <Official></Official>
+        </Col>
+        <NavigationBar />
+        <SecondaryNavagationBar />
+
+        <Slide> </Slide>
+        <Slogan></Slogan>
+        <News></News>
+        <RecruitSlide></RecruitSlide>
+        <NewSection></NewSection>
+        <SlideThree></SlideThree>
+        <SlideTwo></SlideTwo>
+      </Row>
+    </Grid>
   );
 }
 
