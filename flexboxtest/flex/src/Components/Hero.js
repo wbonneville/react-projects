211173c82982c5dbbe310bf3db879e3b44b92050
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-04 15:12:01 -0400

    Update Hero.js

--- flexboxtest/flex/src/Components/Hero.js
+++ flexboxtest/flex/src/Components/Hero.js
@@ -2,8 +2,13 @@ import React, { Component } from "react";
 import image from "../images/teamwork.png";
 import styled from "styled-components";
 
+const HeroWrapper = styled.div`
+  font-family: "Futura";
+`;
+
 const Image = styled.div`
-  background-image: url(${image});
+  background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1)),
+    url(${image});
   background-size: cover;
   margin-top: -16px;
   & .slide {
@@ -11,16 +16,33 @@ const Image = styled.div`
   }
 `;
 
+const Welcome = styled.h1`
+  color: white;
+  font-size: 75px;
+`;
+
+const Slogan = styled.h2`
+  color: white;
+  font-size: 25px;
+`;
+
 export default class Hero extends Component {
   render() {
     return (
-      <Image>
-        <div className="row xs-center">
-          <div className="col-xs-12 ">
-            <div className="slide"></div>
+      <HeroWrapper>
+        <Image>
+          <div className="row">
+            <div className="col-xs-12">
+              <div className="row center-xs slide">
+                <div className="col-xs-12">
+                  <Welcome>Welcome.</Welcome>
+                  <Slogan>It's great to part of the greater good.</Slogan>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-      </Image>
+        </Image>
+      </HeroWrapper>
     );
   }
 }
