6c66cf7498916138fafa67f80435fbe1abb3ac13
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-06 14:52:51 -0400

    Update SectionTwo.js

--- flexboxtest/flex/src/Components/SectionTwo.js
+++ flexboxtest/flex/src/Components/SectionTwo.js
@@ -4,17 +4,37 @@ import styled from "styled-components";
 const SectionWrapper = styled.div`
   height: 800px;
   width: 100%;
+  font-family: "Futura";
 `;
+
+const Watch = styled.div`
+  margin-top: 8%;
+
+  & h1 {
+    font-size: 70px;
+    color: #17263a;
+  }
+  & h4 {
+    color: #17263a;
+  }
+`;
+
+const Video = styled.div``;
 
 export default class SectionTwo extends Component {
   render() {
     return (
       <SectionWrapper>
-        <div className="row center-xs">
+        <Watch className="row center-xs">
           <div className="col-xs-12">
-            <h1>Watch</h1>
+            <h1>Work</h1>
+            <h4>
+              Let us make a double harvest: one of agricultural success and one
+              of ideological reform.
+            </h4>
           </div>
-        </div>
+        </Watch>
+        <Video></Video>
       </SectionWrapper>
     );
   }
