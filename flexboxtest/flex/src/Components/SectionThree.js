4ca10e5dd5d7a34656d14e21e9b67a6ee71208a6
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-08 11:45:09 -0400

    Update SectionThree.js

--- flexboxtest/flex/src/Components/SectionThree.js
+++ flexboxtest/flex/src/Components/SectionThree.js
@@ -4,28 +4,27 @@ import styled from "styled-components";
 const SectionWrapper = styled.div`
   height: 800px;
   width: 100%;
-  font-family: "Futura";
+  font-family: "EB Garamond";
   background-color: #f6f6f6;
 `;
 
-const Watch = styled.div`
-  margin-top: 8%;
-
-  & h1 {
-    font-size: 70px;
-    color: #17263a;
-  }
-  & h4 {
-    color: #17263a;
-  }
-`;
+// font-family: 'Noto Sans', sans-serif;
+// font-family: 'Roboto Slab', serif;
+// font-family: 'Nanum Gothic', sans-serif;
+// font-family: 'EB Garamond', serif;
+// font-family: 'Amiri', serif;
 
 const NewsPanel = styled.div`
   padding-top: 3%;
   padding-bottom: 3%;
+
+  & p {
+    font-size: 20px;
+  }
+
   & a {
-    color: black;
     text-decoration: none;
+    color: inherit;
   }
 `;
 
@@ -34,26 +33,26 @@ export default class SectionTwo extends Component {
     return (
       <React.Fragment>
         <SectionWrapper>
-          <NewsPanel class="container">
-            <div class="row center-xs">
-              <div class="col-xs-12 col-md-10">
-                <h3 class="">
-                  <a class="" href="#">
+          <NewsPanel className="container">
+            <div className="row center-xs">
+              <div className="col-xs-12 col-md-10">
+                <p className="">
+                  <a className="" href="#">
                     "Dear Dr. Breen. Why has the Combine seen fit to suppress
                     our reproductive cycle? Sincerely, A Concerned Citizen."
                   </a>
-                </h3>
+                </p>
               </div>
-              <div class="col-xs-12 col-md-2">
-                <p class="">April 02, 2018</p>
-              </div>
-            </div>
-            <div className="row center-xs">
-              <div className="col-xs-11">
-                <hr></hr>
+              <div className="col-xs-12 col-md-2">
+                <p className="">April 02, 2018</p>
               </div>
             </div>
           </NewsPanel>
+          <div className="row center-xs">
+            <div className="col-xs-11">
+              <hr></hr>
+            </div>
+          </div>
         </SectionWrapper>
       </React.Fragment>
     );
