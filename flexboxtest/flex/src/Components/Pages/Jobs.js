06cabd1494d0d058ab35fd360f7c947bd88b5478
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-11 08:25:19 -0400

    Update Jobs.js

--- flexboxtest/flex/src/Components/Pages/Jobs.js
+++ flexboxtest/flex/src/Components/Pages/Jobs.js
@@ -3,6 +3,10 @@ import styled from "styled-components";
 
 export default class Jobs extends Component {
   render() {
-    return <div>Fruit</div>;
+    return (
+      <div className="row center-xs">
+        <div className="col-xs-12 col-md-6">Jobs and Positions</div>
+      </div>
+    );
   }
 }
