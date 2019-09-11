119e7630a63c88f95ae561823beac87d6d7c9ff5
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-11 08:36:15 -0400

    Update Jobs.js

--- flexboxtest/flex/src/Components/Pages/Jobs.js
+++ flexboxtest/flex/src/Components/Pages/Jobs.js
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 import styled from "styled-components";
 
+const JobsHeader = styled.div`
+  font-size: 24px;
+`;
+
+const JobsInfo = styled.div`
+  font-size: 17px;
+  text-align: left;
+`;
+
 export default class Jobs extends Component {
   render() {
     return (
       <div className="row center-xs">
-        <div className="col-xs-12 col-md-6">Jobs and Positions</div>
+        <JobsHeader className="col-xs-12 col-lg-6">
+          <strong>Jobs and Positions</strong>
+        </JobsHeader>
+        <JobsInfo className="col-xs-12 col-lg-6">
+          What is AI? It's the quest to build machines that can reason, learn,
+          and act intelligently, and it has barely begun. We cover the latest
+          advances in machine learning, neural networks, and robots.
+        </JobsInfo>
       </div>
     );
   }
