e77d89ddf855a2b74a7da42b3e9f531cf0d4ebd7
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-11 15:41:14 -0400

    Update jobs api

--- flexboxtest/flex/src/Components/Pages/Jobs.js
+++ flexboxtest/flex/src/Components/Pages/Jobs.js
@@ -4,7 +4,7 @@ import image1 from "../../images/factory.jpeg";
 import image2 from "../../images/breen.jpg";
 import image3 from "../../images/job.png";
 import image4 from "../../images/soviet.jpg";
-import Epic from "./Api";
+import TestApi from "./JobsApi";
 
 // SECTION 1 STYLES
 
@@ -277,7 +277,7 @@ export default class Jobs extends Component {
             </Message>
           </LatestJobs>
         </SectionFive>
-        <Epic></Epic>
+        <TestApi></TestApi>
       </SectionWrapper>
     );
   }
--- flexboxtest/flex/src/Components/Pages/JobsApi.js
+++ flexboxtest/flex/src/Components/Pages/JobsApi.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 
-export default class Epic extends Component {
+export default class TestApi extends Component {
   constructor(props) {
     super(props);
     this.state = {
