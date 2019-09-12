b05759a05eee63b23badb57346272858a3d4a06a
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-12 11:14:47 -0400

    Update Footer.js

--- flexboxtest/flex/src/Components/Footer.js
+++ flexboxtest/flex/src/Components/Footer.js
@@ -1,11 +1,51 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+
+const FooterWrapper = styled.div`
+  padding: 40px;
+  padding-left: 20%;
+  background: black;
+  color: white;
+`;
 
 export default class Footer extends Component {
   render() {
     return (
-      <div className="row">
-        <div className="col-xs-12"></div>
-      </div>
+      <FooterWrapper>
+        <div className="row">
+          <div className=" col-xs-10">
+            <h1>MIT Technology Review</h1>
+            <p>
+              The mission of MIT Technology Review is to bring about
+              better-informed and more conscious decisions about technology
+              through authoritative, influential, and trustworthy journalism.
+            </p>
+          </div>
+          <div className="col-xs-2">
+            <h2>Browse International Editions</h2>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-2">
+            <h4>yee</h4>
+          </div>
+          <div className="col-xs-2">
+            <h4>yee</h4>
+          </div>
+          <div className="col-xs-2">
+            <h4>yee</h4>
+          </div>
+          <div className="col-xs-2">
+            <h4>yee</h4>
+          </div>
+          <div className="col-xs-2">
+            <h4>yee</h4>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-12"></div>
+        </div>
+      </FooterWrapper>
     );
   }
 }
