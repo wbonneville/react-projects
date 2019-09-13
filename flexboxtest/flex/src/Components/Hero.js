8ccade5a17b88fc051ad8cffb754a3b326f7d50d
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-13 10:54:36 -0400

    Update Hero.js

--- flexboxtest/flex/src/Components/Hero.js
+++ flexboxtest/flex/src/Components/Hero.js
@@ -2,21 +2,10 @@ import React, { Component } from "react";
 import styled from "styled-components";
 import image from "../images/wallhaven.jpg";
 
-const Image = styled.div`
-  background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1)),
-    url(${image});
-  background-size: cover;
-
-  & .slide {
-    height: 550px;
-  }
-`;
-
 const HeroWrapper = styled.div`
   padding-left: 8%;
   padding-right: 8%;
   padding-top: 4%;
-
   background-color: #f6f6f6;
 `;
 
@@ -25,19 +14,27 @@ const HeroRow = styled.div``;
 const Left = styled.div`
   background-color: black;
   color: white;
-  padding: 2%;
-
+  padding: 0 0;
   & h1 {
     font-size: 45px;
     text-align: left;
     font-weight: 800;
+    padding: 2%;
   }
 
   & h4 {
     text-align: left;
     font-size: 20px;
     font-weight: 300;
+    padding: 2%;
   }
+`;
+
+const Image = styled.div`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1)),
+    url(${image});
+  background-size: cover;
+  height: 450px;
 `;
 
 const Right = styled.div``;
@@ -56,9 +53,7 @@ export default class Hero extends Component {
               Signe Swenson, who leaked details about the lab’s relationship
               with Jeffrey Epstein, explains how its funding works.
             </h4>
-            <Image>
-              <div className="slide"></div>
-            </Image>
+            <Image></Image>
           </Left>
           <Right className="col-xs-5">
             <h1>Here</h1>
