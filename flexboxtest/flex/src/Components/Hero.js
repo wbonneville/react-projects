76327cf90311d7205f603c2149a0f5b0a652cb95
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-13 10:34:28 -0400

    Update

--- flexboxtest/flex/src/Components/Hero.js
+++ flexboxtest/flex/src/Components/Hero.js
@@ -1,18 +1,70 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import image from "../images/wallhaven.jpg";
+
+const Image = styled.div`
+  background-image: linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.1)),
+    url(${image});
+  background-size: cover;
+
+  & .slide {
+    height: 550px;
+  }
+`;
+
+const HeroWrapper = styled.div`
+  padding-left: 8%;
+  padding-right: 8%;
+  padding-top: 4%;
+
+  background-color: #f6f6f6;
+`;
+
+const HeroRow = styled.div``;
+
+const Left = styled.div`
+  background-color: black;
+  color: white;
+  padding: 2%;
+
+  & h1 {
+    font-size: 45px;
+    text-align: left;
+    font-weight: 800;
+  }
+
+  & h4 {
+    text-align: left;
+    font-size: 20px;
+    font-weight: 300;
+  }
+`;
+
+const Right = styled.div``;
 
 export default class Hero extends Component {
   render() {
     return (
-      <div>
-        <div className="row center-xs">
-          <div className="col-xs-6">
-            <h1>Hello</h1>
-          </div>
-          <div className="col-xs-6">
+      <HeroWrapper>
+        <HeroRow className="row center-xs">
+          <Left className="col-xs-7">
+            <h1>
+              MIT’s “disqualified” donors aren’t necessarily banned from
+              donating, says Media Lab whistleblower
+            </h1>
+            <h4>
+              Signe Swenson, who leaked details about the lab’s relationship
+              with Jeffrey Epstein, explains how its funding works.
+            </h4>
+            <Image>
+              <div className="slide"></div>
+            </Image>
+          </Left>
+          <Right className="col-xs-5">
             <h1>Here</h1>
-          </div>
-        </div>
-      </div>
+          </Right>
+        </HeroRow>
+      </HeroWrapper>
     );
   }
 }
