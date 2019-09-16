423b5691f3e6fe224b72d43712f4c57d189b8c15
Wesley Bonneville <westazbonneville@gmail.com>
2019-09-16 10:48:34 -0400

    Update Housing.js

--- flexboxtest/flex/src/Components/Pages/Housing.js
+++ flexboxtest/flex/src/Components/Pages/Housing.js
@@ -18,50 +18,71 @@ export default class Housing extends Component {
     return (
       <Div className="row start-xs">
         <div className="col-xs-10">
-          <h1>Lorem ipsum dolor sit.</h1>
+          <h1>Apartments and Housing</h1>
           <h4>September 16th, 2019</h4>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipiscing elit eros morbi,
-            massa mi integer suspendisse molestie parturient purus interdum,
-            facilisis torquent cubilia habitant ornare mus nisl sollicitudin.
-            Tincidunt eleifend quis pharetra feugiat justo egestas suspendisse,
-            aenean ornare lacus porttitor enim commodo fusce, imperdiet quisque
-            eget duis montes id. Litora purus consequat varius magna cursus
-            pretium tempor lobortis sapien, vehicula ut nibh lacus montes
-            tristique vivamus accumsan nam, ridiculus malesuada mus egestas
-            gravida nascetur iaculis sodales. Imperdiet volutpat mi pretium
-            libero nascetur augue tincidunt tempor nec, nisi malesuada purus
-            ante ac rutrum magnis erat, mattis fusce posuere nostra cubilia
-            porttitor nulla odio. Aptent blandit ornare nascetur vulputate
-            fermentum hac et ligula platea at dictumst morbi aliquet duis a,
-            magna vestibulum nullam nibh penatibus mauris nostra sodales ut
-            etiam rutrum iaculis curae in. Ut odio turpis eget ultricies dapibus
-            penatibus donec congue, per imperdiet cursus rutrum dictumst class
-            tincidunt dis, leo nunc duis purus urna himenaeos proin. Ridiculus
-            tortor enim malesuada tempus bibendum phasellus tempor quisque
-            condimentum penatibus, mattis est senectus porta venenatis inceptos
-            nulla iaculis varius, ut sociosqu imperdiet facilisis viverra
-            eleifend non etiam congue. Quisque laoreet semper sapien pharetra
-            elementum condimentum nibh at, malesuada dapibus ante varius
-            lobortis class ultrices sagittis commodo, erat cubilia hendrerit
-            purus sodales vehicula tincidunt. Viverra enim cubilia ligula
-            pretium vulputate dui senectus nulla, massa consequat a mauris
-            mattis scelerisque in, vehicula cursus phasellus facilisis iaculis
-            id malesuada. Laoreet eget neque sociosqu nulla morbi parturient ad,
-            porttitor commodo blandit mus torquent magnis, feugiat id malesuada
-            varius mattis accumsan. Dis malesuada ut in fermentum quisque
-            condimentum proin, auctor curabitur hendrerit hac laoreet nec. Nec
-            risus aliquet tempus bibendum at torquent eget, vulputate enim
-            habitant curabitur vel posuere tristique odio, proin ultrices vitae
-            praesent morbi suspendisse. Purus lobortis tristique neque venenatis
-            fames curabitur viverra pulvinar ut vel tincidunt, leo commodo est
-            malesuada congue habitasse rutrum morbi sociis imperdiet, erat
-            euismod consequat duis posuere feugiat tortor dapibus nec montes. At
-            rutrum accumsan ornare aliquet dictum urna interdum scelerisque
-            tincidunt, cubilia leo venenatis curae nunc habitant tristique
-            ultrices posuere, nostra nec rhoncus iaculis facilisis lobortis
-            penatibus dictumst, sociis eleifend velit torquent aenean ad
-            elementum malesuada.
+            Here's a bit of zaniness that’s destined to infuriate many readers –
+            but I think it’s fascinating, if you don’t mind some healthy
+            skepticism in advance. I don’t have a lot of reason to be suprised
+            by this particular story – it’s true that the technological progress
+            that’s driven overall housing prices up have been mostly effective
+            at putting upward pressure on rents. One key thing to keep in mind,
+            though, is that rents are not necessarily driven to the ceiling by
+            the demand for housing (they can also be driven up by supply).
+            <br></br>
+            <br></br>
+            Consider rents in City 17, which are well above the national
+            average. Since the war, rents have been very volatile across
+            different city-wide measures – but they are moving in tandem with
+            rent prices that are considerably below their wartime peaks. There
+            are many explanations for this, of course. It’s possible, for
+            example, that tech has put a damper on the market. Or that, as at
+            least one group of white-collar workers has discovered, the labor
+            market offers little scope for upward mobility. But maybe, with one
+            big change, one would expect rents to surge. For the past decades,
+            the great decline in homeownership has surely put upward pressure on
+            the demand for rentals. Now that so many people are choosing not to
+            even try to upgrade the degree of housing they have, they will be
+            coming to the rental market, at least on a regular basis.
+            <br></br>
+            <br></br>
+            Still, these sorts of facts are exceedingly rare, which is why
+            skeptics – among them yours truly – often buy into widespread dogma.
+            In part this may be because, when it comes to crises, people want to
+            hang on to a traditional narrative, as opposed to offering new
+            possibilities. But it’s also because people (particularly informed
+            ones) want to hear the familiar “we’re in a housing bubble” alarm.
+            Now here’s the strange part of this – and this is certainly what
+            some of my critics are going to charge. These “bubble” stories
+            refuse to allow that maybe, just maybe, housing is really not
+            well-serviced by technology at the present time. Of course, tech
+            innovation does need some sort of assistance. But I’m not sure that
+            it’s artificial growth that’s the problem – maybe it’s landlords who
+            are refusing to deal with the new technologies. While it may be that
+            landlords are more interested in profit than housing is a good
+            investment, this is not something that’s impossible to measure.
+            <br></br>
+            <br></br>
+            There are various metrics for which rent control can be controlled
+            (a lot of people think it has been a disaster). Meanwhile, in
+            Seattle, digital contracting and deep tech could provide a
+            significant challenge to the local landlord establishment. And as
+            someone who’s actually watched all of these developments in action –
+            because the city’s zoning was written just before rent control was
+            considered – I think that this is a very plausible scenario. But can
+            we convince the Pollyannas to buy into this? After all, the general
+            consensus has been that tech companies were holding back on new
+            development for too long, which is part of what’s leading to all the
+            price increases. Not everyone’s doing this, of course. Of course, it
+            would be nice if the builders and landlords had a particular reason
+            to do things differently – but that would be to demand more.
+            <br></br>
+            <br></br>
+            So maybe the problem is that the realities of technology have turned
+            housing from a good investment to one that barely pays its way. And
+            maybe if that’s the case, we should have plenty of people moving
+            out, renting, and sending everybody back to the government to ask
+            for help. So maybe, just maybe, this is where we want it to be.
           </p>
         </div>
       </Div>
