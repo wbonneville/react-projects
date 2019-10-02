127f653c1fc34807351c451489fa6ec92067c63c
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-02 10:00:30 -0400

    Update map.js

--- reduxproject/reduxproject/src/systems/map.js
+++ reduxproject/reduxproject/src/systems/map.js
@@ -16,7 +16,7 @@ export default class CanvasComponent extends React.Component {
   updateCircles() {
     const ctx = this.refs.canvas.getContext("2d");
     ctx.beginPath();
-    for (var i = 0; i < 80; i++) {
+    for (var i = 0; i < 300; i++) {
       const x = Math.floor(Math.random() * this.refs.canvas.width);
       const y = Math.floor(Math.random() * this.refs.canvas.height);
       ctx.moveTo(x, y);
