57ee43b84d7a6e74de74918f394d0a9ecd089a4c
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-29 12:00:32 -0400

    Update Product.js

--- store/store/src/components/Product.js
+++ store/store/src/components/Product.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+import { ProductConsumer } from '../context';
 
 export default class Product extends Component {
   render() {
-    return <div>Hello from Product</div>;
+    const { id, title, img, price, inCart } = this.props.product;
+
+    return (
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        Hello from Product
+      </ProductWrapper>
+    );
   }
 }
+
+const ProductWrapper = styled.div``;
