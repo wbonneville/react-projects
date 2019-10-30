79eff4d7231c9c2616c175c4bbaa7267b6131220
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-30 12:43:25 -0400

    Add props

--- store/store/src/components/Details.js
+++ store/store/src/components/Details.js
@@ -53,6 +53,7 @@ export default class Details extends Component {
                       <ButtonContainer>Back to products</ButtonContainer>
                     </Link>
                     <ButtonContainer
+                      cart
                       disabled={inCart ? true : false}
                       onClick={() => {
                         value.addToCart(id);
