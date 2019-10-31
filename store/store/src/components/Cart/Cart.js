ddc7d72d303178bcdc2b17c7771805575b768384
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-31 12:39:32 -0400

    Add cart column component

--- store/store/src/components/Cart/Cart.js
+++ store/store/src/components/Cart/Cart.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
+import Title from '../Title';
+import CartColumns from './CartColumns';
 
 export default class Cart extends Component {
   render() {
-    return <div>Hello from cart</div>;
+    return (
+      <section>
+        <Title name="your" title="cart" />
+        <CartColumns></CartColumns>
+      </section>
+    );
   }
 }
---
+++ store/store/src/components/Cart/CartColumns.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+
+export default class CartColumns extends Component {
+  render() {
+    return <div></div>;
+  }
+}
