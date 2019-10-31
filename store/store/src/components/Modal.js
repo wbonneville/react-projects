e821ecd8954fa5204e75c6849a41dea8525a880f
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-31 11:25:02 -0400

    Add links

--- store/store/src/components/Modal.js
+++ store/store/src/components/Modal.js
@@ -24,6 +24,18 @@ export default class Modal extends Component {
                     >
                       <h5>item added to the cart</h5>
                       <img src={img} className="img-fluid" alt="product" />
+                      <h5>{title}</h5>
+                      <h5 className="text-muted">price : ${price}</h5>
+                      <Link to="/">
+                        <ButtonContainer onClick={() => closeModal()}>
+                          Store
+                        </ButtonContainer>
+                      </Link>
+                      <Link to="/cart">
+                        <ButtonContainer cart onClick={() => closeModal()}>
+                          Go To Cart
+                        </ButtonContainer>
+                      </Link>
                     </div>
                   </div>
                 </div>
