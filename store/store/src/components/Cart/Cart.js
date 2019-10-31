7e6b52934248648e090b9ba18ba5b9a7e6b90c0f
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-31 12:48:06 -0400

    Create empty cart

--- store/store/src/components/Cart/Cart.js
+++ store/store/src/components/Cart/Cart.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Title from '../Title';
 import CartColumns from './CartColumns';
+import EmptyCart from './EmptyCart';
 
 export default class Cart extends Component {
   render() {
