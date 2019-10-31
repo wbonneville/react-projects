8f02fbed2afb76f6d55e7517d54101fbaf509540
Wesley Bonneville <westazbonneville@gmail.com>
2019-10-31 10:51:46 -0400

    Add modal product consumer

--- store/store/src/App.js
+++ store/store/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import logo from './logo.svg';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
