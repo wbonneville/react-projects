60b0ae9ac06878a7750b05512b92bf6bbbcb0d18
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-01 11:41:24 -0400

    Update CartItem.js

--- store/store/src/components/Cart/CartItem.js
+++ store/store/src/components/Cart/CartItem.js
@@ -4,7 +4,7 @@ export default function CartItem({ item, value }) {
   const { id, title, img, price, total, count } = item;
   const { increment, decrement, removeItem } = value;
   return (
-    <div className="row my-1 text-capitalize text-center">
+    <div className="row my-2 text-capitalize text-center">
       <div className="col-10-mx-auto col-lg-2">
         <img
           src={img}
@@ -14,10 +14,10 @@ export default function CartItem({ item, value }) {
         />
       </div>
       <div className="col-10-mx-auto col-lg-2">
-        <span className="d-lg-none">product: {title}</span>
+        <span className="d-lg-none">product: </span> {title}
       </div>
       <div className="col-10-mx-auto col-lg-2">
-        <span className="d-lg-none">price: {price}</span>
+        <span className="d-lg-none">price: </span> {price}
       </div>
       <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
         <div className="d-flex justify-content-center">
@@ -33,6 +33,14 @@ export default function CartItem({ item, value }) {
         </div>
       </div>
       {/*  */}
+      <div className="col-10-mx-auto col-lg-2">
+        <div className="cart-icon" onClick={() => removeItem(id)}>
+          <i className="fas fa-trash"></i>
+        </div>
+      </div>
+      <div className="col-10-mx-auto col-lg-2">
+        <strong>item total : ${total}</strong>
+      </div>
     </div>
   );
 }
