5985dca6589e2586828054d3e56c183a8a57886b
Wesley Bonneville <westazbonneville@gmail.com>
2019-11-01 14:47:00 -0400

    Add remove item functionality

--- store/store/src/context.js
+++ store/store/src/context.js
@@ -66,7 +66,7 @@ class ProductProvider extends Component {
         return { products: tempProducts, cart: [...this.state.cart, product] };
       },
       () => {
-        console.log(this.state);
+        this.addTotals();
       },
     );
   };
@@ -97,11 +97,49 @@ class ProductProvider extends Component {
   };
 
   removeItem = id => {
-    console.log('item removed');
+    let tempProducts = [...this.state.products];
+    let tempCart = [...this.state.cart];
+    tempCart = tempCart.filter(item => item.id !== id);
+    const index = tempProducts.indexOf(this.getItem(id));
+    let removedProduct = tempProducts[index];
+    removedProduct.inCart = false;
+    removedProduct.count = 0;
+    removedProduct.total = 0;
+
+    this.setState(
+      () => {
+        return {
+          cart: [...tempCart],
+          products: [...tempProducts],
+        };
+      },
+      () => {
+        this.addTotals();
+      },
+    );
   };
 
   clearCart = id => {
-    console.log('cart was cleared');
+    this.setState(
+      () => {
+        return { cart: [] };
+      },
+      () => {
+        this.setProducts();
+        this.addTotals();
+      },
+    );
+  };
+
+  addTotals = () => {
+    let subTotal = 0;
+    this.state.cart.map(item => (subTotal += item.total));
+    const tempTax = subTotal * 0.1;
+    const tax = parseFloat(tempTax.toFixed(2));
+    const total = subTotal + tax;
+    this.setState(() => {
+      return { cartSubTotal: subTotal, cartTax: tax, cartTotal: total };
+    });
   };
 
   render() {
